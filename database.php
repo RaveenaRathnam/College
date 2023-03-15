5c6144f993793d7ad7e441d61fc5222569aaf6c5
Raveena Rathnam <D00251856@student.dkit.ie>
2023-03-15 11:58:54 +0000

    updated the database.php

--- database.php
+++ database.php
@@ -2,7 +2,9 @@
     $dsn = 'mysql:host=localhost;dbname=college_php';
     $username = 'root';
     $password = '';
-
+    // $dsn = 'mysql:host=localhost;dbname=D00251856';
+    // $username = 'D00251856';
+    // $password = '6eRP3xbK';
     try {
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
